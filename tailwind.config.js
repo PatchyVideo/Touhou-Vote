c08e6976fb27b3a80d9927837253e1aa639c8561
wrzrmzx <1510033076@qq.com>
2020-07-26 20:12:06 +0800

    ci: :wrench: update tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+    },
+  },
   variants: {},
   plugins: [],
   purge: {
