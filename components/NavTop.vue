59f120bf6a10685b06927e26b086a2541b5c991e
wrzrmzx <1510033076@qq.com>
2020-07-26 20:17:34 +0800

    feat(navigator): :tada: add NavTop.vue

---
+++ components/NavTop.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="NavTop">
+    第九届东方Project人气投票
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="postcss" scoped></style>
