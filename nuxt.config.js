9a4fa83da04acde5d923bcbda4426b4b02cc2b4c
wrzrmzx <1510033076@qq.com>
2020-07-30 15:52:42 +0800

    refactor(global): :fire: delet global.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   /*
    * Global CSS
    */
-  css: ['@/assets/css/global.css'],
+  css: [],
   /*
    * Plugins to load before mounting the App
    * https://nuxtjs.org/guide/plugins
