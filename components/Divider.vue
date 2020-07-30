584aa5bdabf4fe521ad96ba262f9492454e90155
wrzrmzx <1510033076@qq.com>
2020-07-30 15:54:48 +0800

    refactor(others): :lipstick: add component:Divider.vue

---
+++ components/Divider.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Divider">
+    <div class="w-full h-1 bg-gray-900"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="postcss" scoped>
+.Divider {
+  @apply w-full;
+  @apply my-4;
+}
+</style>
