4ce1811a15bc44c9b8c4788ca2b17fb36b559703
wrzrmzx <1510033076@qq.com>
2020-08-02 23:37:32 +0800

    refactor(home): :construction: 新页面：主页

---
+++ pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-4/5 bg-gray-100 p-40 m-auto">
+    <div>人妖部门</div>
+    <div>作品部门</div>
+    <div>音乐部门</div>
+    <div>CP部门</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="postcss" scoped></style>
