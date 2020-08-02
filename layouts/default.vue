c7eaab839c15bad794eb5b057369738122c6a88c
wrzrmzx <1510033076@qq.com>
2020-08-02 23:36:53 +0800

    refactor: 默认布局修改

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+    <!-- 背景图片 -->
+    <div class="BgImg fixed w-screen h-screen bg-cover -z-1">
+      <div class="h-full w-full bg-white opacity-75"></div>
+    </div>
+    <navtop class="fixed top-0 w-full" />
     <Nuxt />
+    <navbottom />
   </div>
 </template>
+<script lang="ts">
+import navtop from '~/components/NavTop.vue'
+import navbottom from '~/components/NavBottom.vue'
+
+export default {
+  components: { navtop, navbottom },
+}
+</script>
+<style lang="postcss" scoped>
+.BgImg {
+  background-image: url('/img/UserLogin.jpg');
+  -webkit-filter: blur(10px);
+  filter: blur(10px);
+}
+</style>
