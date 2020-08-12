04eddbea503abf412dbf8026af2dbab122408232
wrzrmzx <1510033076@qq.com>
2020-08-12 23:13:23 +0800

    refactor(global): :lipstick: new component BaseButton.vue

---
+++ components/BaseButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="Button">
+    <button type="button">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+
+export default defineComponent({
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  setup() {
+    return {}
+  },
+  mounted() {},
+})
+</script>
+
+<style lang="postcss" scoped>
+.Button {
+  @apply inline-block;
+}
+button {
+  @apply w-full;
+  @apply bg-base-green;
+  @apply text-dark-white;
+  @apply font-bold;
+  @apply py-1;
+  @apply px-4;
+  @apply rounded;
+  transition: ease-in-out 0.2s;
+}
+button:hover {
+  @apply bg-dark-green;
+}
+button:focus {
+  @apply outline-none;
+  @apply shadow-outline;
+}
+</style>
