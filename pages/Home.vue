33bdd099ad3876fe6c1411505b7e27239dbe017e
wrzrmzx <1510033076@qq.com>
2020-08-23 23:01:49 +0800

    refactor(home): :lipstick: update UI of home page

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-4/5 bg-gray-100 p-40 m-auto">
-    <div>人妖部门</div>
-    <div>作品部门</div>
-    <div>音乐部门</div>
-    <div>CP部门</div>
+  <div class="w-4/5 bg-gray-100 p-40 m-auto flex flex-col">
+    <p>朋友，在投票前请先填写一下问卷吧！</p>
+    <nuxt-link class="voteLink" to="/VotepageList">调查问卷</nuxt-link>
+    <nuxt-link class="voteLink" to="/VotepageCharacter">人妖部门</nuxt-link>
+    <nuxt-link class="voteLink" to="/VotepageTitle">作品部门</nuxt-link>
+    <nuxt-link class="voteLink" to="/VotepageMusic">音乐部门</nuxt-link>
+    <nuxt-link class="voteLink" to="/VotepageCouple">CP部门</nuxt-link>
   </div>
 </template>
 
@@ -17,4 +19,12 @@ export default defineComponent({
 })
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.voteLink {
+  @apply text-4xl;
+  @apply my-2;
+}
+.voteLink:hover {
+  @apply bg-gray-200;
+}
+</style>
