28025b58e884e7cb3e58bcefaebb71f5aee6064b
QiroNT <i@ntz.im>
2021-08-15 21:01:53 +0800

    chore: fill some content

--- packages/home/Home.vue
+++ packages/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid w-full h-full place-items-center">
-    <div class="grid w-full h-full place-items-center">
-      <div class="w-full h-100vh">
+  <div class="page grid w-full h-full max-w-screen-3xl mx-auto place-items-center">
+    <div class="w-full max-w-screen-2xl mx-auto">
+      <div class="w-full h-100vh max-h-screen-lg">
         <div class="title-page w-full h-full">
           <div class="w-full h-full backdrop-filter backdrop-blur-1">
-            <div class="grid w-full max-w-screen-2xl h-full place-items-center mx-auto">
+            <div class="grid w-full h-full place-items-center mx-auto">
               <div class="flex w-4/5 flex-row justify-between">
                 <div>
                   <div class="quicksand text-3xl text-gray-800">
@@ -47,7 +47,7 @@
                         ></path></svg></span
                     >PatchyVideo
                   </div>
-                  <div class="mt-16 h-15 w-128 font-sans font-thin">
+                  <div class="mt-10 h-15 w-128">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.075 1.294">
                       <path
                         d="M.944.994H.216q-.03 0-.03-.03 0-.029.03-.029h.318V.653H.281q-.03 0-.03-.032 0-.03.03-.03h.254V.35H.24Q.21.35.21.318q0-.03.03-.03h.671q.03 0 .03.03 0 .032-.03.032H.598v.241h.277q.032 0 .032.03 0 .032-.032.032H.598v.281h.346q.03 0 .03.029 0 .03-.03.03zM.853.876Q.791.798.734.748.712.73.733.714.754.696.773.712.874.809.901.846.914.864.89.881.868.897.853.876zM1.054.14H.091Q.07.141.07.161v.973q0 .019.021.019h.963q.021 0 .021-.019V.161q0-.021-.021-.021zM.061.077h1.027q.026 0 .041.017.017.015.017.041V1.16q0 .026-.017.041-.015.017-.041.017H.061q-.028 0-.043-.017-.015-.015-.015-.041V.135q0-.026.015-.041Q.033.077.061.077zM2.02.254h.458q.028 0 .043.017.015.015.015.041v.868q0 .041-.012.062-.012.022-.043.032-.041.012-.205.012-.048 0-.058-.03-.014-.04.052-.04.107.004.167 0 .021-.001.028-.01.007-.007.007-.026V.34q0-.021-.019-.021H2.02q-.004.11-.03.201.248.215.408.397.018.022-.011.044-.029.022-.044 0Q2.22.81 1.971.584q-.087.233-.328.386-.037.022-.056.001Q1.575.955 1.583.94q.01-.017.032-.029.101-.063.17-.139.069-.077.102-.157.033-.081.045-.147Q1.946.4 1.949.32H1.52q-.019 0-.019.021v.916q0 .033-.033.033-.033 0-.033-.033V.311q0-.026.015-.041.017-.017.043-.017h.458V.044q0-.034.036-.034.034 0 .034.034z"
@@ -66,8 +66,8 @@
                       />
                     </svg>
                   </div>
-                  <div class="ml-6 mt-2 h-12 w-37 font-sans font-light">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4.028 1.315">
+                  <div class="flex ml-6 mr-8 mt-2 flex-row justify-between">
+                    <svg class="h-12 w-37" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4.028 1.315">
                       <path
                         d="M.206.147h.371q.03 0 .03.03 0 .029-.03.029H.319q.03.065.048.125.006.025-.023.034-.033.01-.04-.018Q.291.294.253.207h-.08Q.122.299.06.368.043.389.018.372-.011.351.012.325.113.216.18.043.191.014.222.022.256.03.242.063.226.103.206.147zM.2.857h.393V.7H.237Q.219.788.2.857zm.835-.393H.66v.175h.375q.019 0 .019-.021V.484q0-.019-.019-.019zM.138.404h.923q.028 0 .043.017.015.015.015.041v.181q0 .028-.015.043Q1.088.7 1.061.7H.66v.157h.482q.061 0 .048.106-.025.205-.07.244-.028.028-.077.028-.052.003-.136-.001-.041-.003-.045-.034L.86 1.198q0-.001 0-.003-.011-.028.026-.023.059.004.132.004.039 0 .051-.012.033-.029.054-.209.006-.036-.015-.036H.66v.345q0 .033-.033.033-.034 0-.034-.033V.933q-.095.101-.241.187-.145.087-.292.139-.026.008-.043-.011-.029-.034.017-.048.136-.044.27-.12.134-.076.222-.163H.183q0 .003-.003.011Q.179.936.178.937q0 .001-.003.01Q.173.954.172.955.171.955.168.962.165.967.162.967.16.966.156.97.151.974.147.972.143.969.136.97.102.963.11.936.14.839.173.686.183.639.234.639h.358V.464H.138q-.03 0-.03-.032 0-.029.03-.029zM.74.147h.485q.03 0 .03.03 0 .029-.03.029H.901q.054.068.081.13.011.025-.017.037Q.934.386.923.361.891.288.828.207H.704Q.649.292.573.365.55.387.525.368.493.343.526.316.656.196.719.044.73.014.76.022.798.03.783.065.766.106.74.147z"
                       />
@@ -75,12 +75,25 @@
                         d="M2.694.657q0 .273-.193.466-.193.192-.464.192-.271 0-.464-.193Q1.38.929 1.38.658q0-.271.193-.464Q1.765 0 2.037 0q.271 0 .464.193.193.193.193.464zM1.602.225q-.178.179-.178.433 0 .254.179.433.179.179.433.179.254 0 .433-.178.179-.179.179-.434 0-.255-.179-.434-.179-.18-.432-.18-.255 0-.434.181zm.251.096v.638q0 .055-.055.055-.055 0-.055-.055V.416H1.66q-.037 0-.037-.04 0-.015.011-.028.011-.014.026-.017.04-.011.083-.033.025-.015.058-.015h.014q.039 0 .039.037zm.528.606q-.062.103-.165.103-.105 0-.167-.103-.062-.103-.062-.281 0-.178.062-.278.062-.102.167-.102.105 0 .165.102.062.101.062.278 0 .179-.062.281zM2.132.865q.03.072.084.072.054 0 .085-.073.032-.074.032-.218 0-.143-.032-.215Q2.269.36 2.216.36q-.054 0-.085.072-.032.072-.032.215 0 .136.033.219zM3.267.834h.356q.019 0 .019-.019V.484q0-.019-.019-.019h-.356q-.019 0-.019.019v.331q0 .019.019.019zm.382.062h-.408q-.056 0-.056-.058v-.38q0-.056.056-.056h.408q.058 0 .058.056v.38q0 .028-.015.043-.015.015-.043.015zm.284-.747h-.97q-.021 0-.021.021v.965q0 .019.021.019h.97q.021 0 .021-.019V.17q0-.021-.021-.021zm-1-.068h1.032q.026 0 .041.017.017.015.017.041V1.16q0 .028-.017.043-.015.015-.041.015H2.932q-.028 0-.043-.015-.015-.015-.015-.043V.139q0-.026.015-.041.015-.017.043-.017z"
                       />
                     </svg>
+                    <div class="text-right text-gray-700 font-sans">距离投票结束还有 00天00时00分</div>
+                  </div>
+                  <div class="mt-8 w-128 text-gray-600 text-xl font-light">
+                    这是一个为了调查东方Project系列在中文圈的大致情况而举办的一次调查活动。在活动期间，我们同往届一样，接受来自中文圈内的东方爱好者们的投票，并在投票结束后择日公布本次投票的结果。敬请期待。
                   </div>
                 </div>
                 <div>
-                  <div class="w-146 shadow-xl rounded-3xl bg-white bg-opacity-80 backdrop-filter backdrop-blur-4">
+                  <div
+                    class="
+                      w-146
+                      shadow-xl
+                      rounded-3xl
+                      bg-white bg-opacity-80
+                      backdrop-filter backdrop-blur-4
+                      border-2 border-white border-opacity-20
+                    "
+                  >
                     <div class="px-10 py-8 w-full h-full">
-                      <div class="text-lg text-gray-500">登录一个账户 开始参与投票吧！</div>
+                      <div class="text-lg text-gray-700 font-light">登录一个账户 开始参与投票吧！</div>
                       <div class="mt-2 flex flex-row space-x-6">
                         <div class="inline-block mt-2 px-4 pt-2 pb-1 text-gray-800 border border-gray-300 rounded-lg">
                           <img class="inline-block w-8 h-9 pb-1 align-middle" src="https://thwiki.cc/favicon.ico" />
@@ -125,7 +138,7 @@
                         </div>
                       </div>
                       <div class="mt-6 mb-5 w-full border-dotted border-t border-gray-400"></div>
-                      <div class="text-lg text-gray-500">使用既有人气投票账户登录</div>
+                      <div class="text-lg text-gray-700 font-light">使用既有人气投票账户登录</div>
                       <label class="input-border flex flex-row mt-5 py-2.5 px-4 rounded-xl"
                         ><icon-uil-user class="inline-block w-6 h-6 text-gray-700" /><input
                           class="ml-2 w-full bg-transparent focus:outline-none"
@@ -169,28 +182,82 @@
           </div>
         </div>
       </div>
-      <div class="w-full h-100vh bg-blue-400">
-        <div class="grid w-full h-100vh grid-cols-[0.4fr,1fr,1fr,1fr,0.4fr] gap-12 place-items-center">
+      <div class="w-full h-full py-24">
+        <div class="grid w-full h-full grid-cols-[0.4fr,1fr,1fr,1fr,0.4fr] gap-12 place-items-center">
           <div></div>
-          <div v-for="i in 3" :key="i" class="card-box grid pt-4 place-items-center">
+          <div class="card-box grid pt-4 place-items-center">
             <div class="card-out rounded-md overflow-hidden transform transition-transform duration-450 ease-out">
-              <div class="card-in shadow-md pt-6 pb-2 w-full rounded-md border-4 border-gray-300 blur-2px">
+              <div class="card-in shadow-md pt-6 pb-2 w-full rounded-md border-4 border-blue-50 blur-2px">
                 <div class="m-2">
                   <img
                     class="w-full h-full"
                     src="https://upload.thwiki.cc/a/a6/THBWiki-LOGO-%E5%8D%9A%E4%B8%BD%E7%81%B5%E6%A2%A6%E6%96%B0%E4%BD%9C.png"
                   />
                 </div>
-                <h1 class="m-2 mt-4 text-4xl text-center">参与投票</h1>
-                <div class="m-2 text-center">投票开始时间 0000-00-00</div>
+                <h1 class="m-2 mt-4 text-4xl text-center">重填问卷</h1>
+                <div class="m-2 text-center">重新填写/修改问卷答案</div>
+              </div>
+            </div>
+          </div>
+          <div class="card-box grid pt-4 place-items-center">
+            <div class="card-out rounded-md overflow-hidden transform transition-transform duration-450 ease-out">
+              <div class="card-in shadow-md pt-6 pb-2 w-full rounded-md border-4 border-blue-50 blur-2px">
+                <div class="m-2">
+                  <img
+                    class="w-full h-full"
+                    src="https://upload.thwiki.cc/f/f7/THBWiki-LOGO-%E5%AE%87%E4%BD%90%E8%A7%81%E5%A0%87%E5%AD%90.png"
+                  />
+                </div>
+                <h1 class="m-2 mt-4 text-4xl text-center">修改信息</h1>
+                <div class="m-2 text-center">修改账户信息/账户绑定</div>
+              </div>
+            </div>
+          </div>
+          <div class="card-box grid pt-4 place-items-center">
+            <div class="card-out rounded-md overflow-hidden transform transition-transform duration-450 ease-out">
+              <div class="card-in shadow-md pt-6 pb-2 w-full rounded-md border-4 border-blue-50 blur-2px">
+                <div class="m-2">
+                  <img
+                    class="w-full h-full"
+                    src="https://upload.thwiki.cc/d/dd/THBWiki-LOGO-%E6%9C%AC%E5%B1%85%E5%B0%8F%E9%93%83.png"
+                  />
+                </div>
+                <h1 class="m-2 mt-4 text-4xl text-center">往届结果</h1>
+                <div class="m-2 text-center">查看往届人气投票结果</div>
               </div>
             </div>
           </div>
           <div></div>
         </div>
       </div>
+      <div class="w-3/4 h-full mx-auto py-12">
+        <div class="w-full h-full p-8 rounded-xl bg-white bg-opacity-50 backdrop-filter backdrop-blur-2">
+          <article class="prose-xl">
+            <h2>其他信息</h2>
+            <p>投票的举办地点与结果的发布地点均为本站。</p>
+            <p>
+              本次投票的规则与上一届相同。 <strong>角色票为8个，音乐票为12个。</strong><br />
+              投票过程为黑箱投票，即在投票过程中不显示当前总体投票情况，而是在投票结束后计算出结果进行展示。
+            </p>
+            <p>
+              如果想了解投票活动的最新动态的话，请关注<strong>东方人气投票组委会</strong>：<a
+                class="border-b border-blue-600"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://weibo.com/touhouvote"
+                >新浪微博</a
+              >，或者收藏本站。
+            </p>
+            <p>
+              为了防止刷票等影响公平的行为出现，我们将会采取一切我们认为必要的手段，这一点还请各位投票者注意。<br />
+              希望大家能够在公平公正的原则下，为你所喜欢的项目投上属于你的一票。
+            </p>
+            <!-- ... -->
+          </article>
+        </div>
+      </div>
       <div class="quicksand text-center my-6">
-        <div>&copy; Copyright 2021 VoileLabs. Licensed under GPL-3.0.</div>
+        <div>&copy; Copyright 2021 VoileLabs, THBWiki. Licensed under GPL-3.0.</div>
         <div>
           Thanks
           <template
@@ -226,8 +293,16 @@
   font-family: 'Quicksand';
 }
 
+.page {
+  background-image: radial-gradient(
+    circle at top left,
+    transparent 0 min(60vw, 1152px),
+    theme('colors.pink.200') min(60vw, 1152px),
+    theme('colors.pink.100')
+  );
+}
+
 .title-page {
-  background-image: repeating-linear-gradient(100deg, transparent 0 47%, theme('colors.pink.200') 47% 100%);
   .input-border {
     box-shadow: inset 0 0 0 2px theme('colors.gray.200');
     position: relative;
@@ -269,11 +344,11 @@
     left: -160%;
     width: 60%;
     height: 100%;
-    background: rgba(255, 255, 255, 0.2);
+    background: rgba(239, 246, 255, 0.3);
     transform: skew(45deg);
   }
   .card-in {
-    background-color: rgba(255, 255, 255, 0.15);
+    background-color: rgba(239, 246, 255, 0.1);
   }
 }
 .card-box:hover {
