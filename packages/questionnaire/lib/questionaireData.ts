85ce5a2dc9a8a43bb27261979f4b1d813e79f897
wrzrmzx <1510033076@qq.com>
2021-10-31 21:54:10 +0800

    chore: :construction: update question data WIP

--- packages/questionnaire/lib/questionaireData.ts
+++ packages/questionnaire/lib/questionaireData.ts
@@ -27,6 +27,42 @@ interface QuestionaireData {
   mainQuestionaire: MainQuestionaire
   [key: string]: MainQuestionaire
 }
+function IDToBigQuestionaire(ID: number): string {
+  while (ID > 10) {
+    ID = Math.floor(ID / 10)
+  }
+  if (ID === 1) return 'mainQuestionaire'
+  else return 'extraQuestionaire'
+}
+function IDToSmallQuestionaire(ID: number): string {
+  while (ID > 100) {
+    ID = Math.floor(ID / 10)
+  }
+  switch (ID) {
+    case 11:
+      return 'requiredQuestionaire'
+    case 12:
+      return 'optionalQuestionaire1'
+    case 13:
+      return 'optionalQuestionaire2'
+    case 21:
+      return 'exQuestionaire1'
+    case 22:
+      return 'exQuestionaire2'
+    case 23:
+      return 'exQuestionaire3'
+    case 24:
+      return 'exQuestionaire4'
+    default:
+      return ''
+  }
+}
+function IDToQuestionLibrary(ID: number): number {
+  while (ID > 1000) {
+    ID = Math.floor(ID / 10)
+  }
+  return Math.floor(ID / 100)
+}
 
 export const questionaireData = ref<QuestionaireData>({
   mainQuestionaire: {
@@ -57,11 +93,21 @@ export const questionaireComputed = ref<QuestionaireALL>(computeQuestionaire())
 
 // 根据用户的投票数据计算出的问卷数据
 export function computeQuestionaire(): QuestionaireALL {
-  const questionaireReturn = {}
+  const questionaireReturn = questionaire
   for (const bigQuestionaire in questionaire) {
     for (const smallQuestionaire in questionaire[bigQuestionaire]) {
-      for (const question in questionaire[bigQuestionaire][smallQuestionaire]) {
-        console.log('1')
+      for (const questionLibrary of questionaire[bigQuestionaire][smallQuestionaire].questions) {
+        if (!questionLibrary.length) return questionaireReturn
+        const answerTarget = questionaireData.value[bigQuestionaire][smallQuestionaire].answers.find(
+          (answer) => Math.floor(answer.id / 10) === Math.floor(questionLibrary[0].id / 10)
+        )
+        const questionTarget = questionLibrary.find((question) => question.id === answerTarget?.id)
+        if (!answerTarget?.options || !questionTarget) continue
+        for (const option of answerTarget.options) {
+          for (const relatedQuestionID of questionTarget.options[option].related) {
+            questionaire[IDToBigQuestionaire(relatedQuestionID)][IDToSmallQuestionaire(relatedQuestionID)].questions
+          }
+        }
       }
     }
   }
