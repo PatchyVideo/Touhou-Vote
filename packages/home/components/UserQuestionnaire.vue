f8ac3245bbd065a44b928776e19e8320e3772642
wrzrmzx <1510033076@qq.com>
2021-11-30 18:04:36 +0800

    refactor: :recycle: refactor user questionnaire page

--- packages/home/components/UserQuestionnaire.vue
+++ packages/home/components/UserQuestionnaire.vue
@@ -2,7 +2,11 @@
   <div class="min-h-100vh page md:min-h-0">
     <!-- Main Content -->
     <div class="w-full p-3 space-y-2">
-      <div class="flex w-full p-0.5 shadow rounded bg-white bg-opacity-50 backdrop-filter backdrop-blur-2">
+      <div
+        v-for="(questionaire, index) in questionnaireKeyToName"
+        :key="index"
+        class="flex w-full p-0.5 shadow rounded bg-white bg-opacity-50 backdrop-filter backdrop-blur-2"
+      >
         <div class="w-1/3 p-0.5 overflow-hidden rounded">
           <div class="w-full aspect-1/1">
             <img src="@/home/assets/DefaultAvatar.jpg" class="object-cover rounded" />
@@ -10,33 +14,12 @@
         </div>
         <div class="w-2/3 p-0.5 flex flex-wrap content-between">
           <div class="w-full space-y-0.5">
-            <div class="text-xl">主问卷</div>
+            <div class="text-base">{{ questionaire.name }}</div>
           </div>
           <div class="w-full text-right">
             <button
-              class="px-2 py-0.5 text-sm rounded text-white bg-accent-color-600 md:px-3 md:py-2"
-              @click="mainQuestionnaire()"
-            >
-              开始填写
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div class="flex w-full p-0.5 shadow rounded bg-white bg-opacity-50 backdrop-filter backdrop-blur-2">
-        <div class="w-1/3 p-0.5 overflow-hidden rounded">
-          <div class="w-full aspect-1/1">
-            <img src="@/home/assets/DefaultAvatar.jpg" class="object-cover rounded" />
-          </div>
-        </div>
-        <div class="w-2/3 p-0.5 flex flex-wrap content-between">
-          <div class="w-full space-y-0.5">
-            <div class="text-xl">额外问卷</div>
-          </div>
-          <div class="w-full text-right">
-            <button
-              class="px-2 py-0.5 text-sm rounded text-white bg-accent-color-600 md:px-3 md:py-2"
-              @click="extraQuestionnaire()"
+              class="px-2 py-0.5 text-sm rounded text-white bg-accent-color-600 xl:px-3 xl:py-2"
+              @click="gotoQuestionnaire(questionaire.bigQuestionnaire, questionaire.smallQuestionnaire)"
             >
               开始填写
             </button>
@@ -56,6 +39,7 @@
 
 <script lang="ts" setup>
 import { useRoute, useRouter } from 'vue-router'
+import { questionnaireKeyToName } from '@/questionnaire/lib/questionnaireData'
 
 const props = defineProps({
   deskTopReturn: {
@@ -73,13 +57,10 @@ function returnBack(): void {
   router.push({ path: route.path, query })
 }
 
-function mainQuestionnaire(): void {
-  router.push('/questionnaire')
-}
-function extraQuestionnaire(): void {
+function gotoQuestionnaire(bigQuestionnaire: string, smallQuestionnaire: string): void {
   router.push({
     path: '/questionnaire',
-    query: { bigQuestionnaire: 'extraQuestionnaire', smallQuestionnaire: 'exQuestionnaire1' },
+    query: { bigQuestionnaire: bigQuestionnaire, smallQuestionnaire: smallQuestionnaire },
   })
 }
 </script>
