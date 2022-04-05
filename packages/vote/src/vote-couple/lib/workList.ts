0bce978f18bd58f6ea84e3b485b9a65aa2cbf2c8
Lurantis <1510033076@qq.com>
2022-04-05 11:39:39 +0800

    chore(vote): :speech_balloon: update vote range of character

--- packages/vote/src/vote-couple/lib/workList.ts
+++ packages/vote/src/vote-couple/lib/workList.ts
@@ -2,12 +2,12 @@ import { computed, ref } from 'vue'
 
 interface Work {
   name: string
-  kind: 'old' | 'new' | 'CD' | 'book' | ''
+  kind: 'old' | 'new' | 'CD' | 'book' | 'others' | ''
 }
 
 interface SelectList {
   name: string
-  value: 'old' | 'new' | 'CD' | 'book' | ''
+  value: 'old' | 'new' | 'CD' | 'book' | 'others' | ''
 }
 
 const works: Work[] = [
@@ -37,6 +37,7 @@ const works: Work[] = [
   { name: '东方鬼形兽', kind: 'new' },
   { name: '东方刚欲异闻', kind: 'new' },
   { name: '东方虹龙洞', kind: 'new' },
+  { name: '蓬莱人形', kind: 'CD' },
   { name: '莲台野夜行', kind: 'CD' },
   { name: '旧约酒馆', kind: 'CD' },
   { name: '东方求闻史纪', kind: 'book' },
@@ -47,7 +48,7 @@ const works: Work[] = [
   { name: '东方铃奈庵', kind: 'book' },
   { name: '东方智灵奇传', kind: 'book' },
   { name: '东方醉蝶华', kind: 'book' },
-  { name: '其他', kind: '' },
+  { name: '其他', kind: 'others' },
 ]
 
 export const kinds: SelectList[] = [
@@ -55,7 +56,7 @@ export const kinds: SelectList[] = [
   { name: '新作', value: 'new' },
   { name: 'CD', value: 'CD' },
   { name: '出版物', value: 'book' },
-  { name: '其他', value: '' },
+  { name: '其他', value: 'others' },
 ]
 
 export const filterForKind = ref<SelectList[]>([
@@ -63,14 +64,14 @@ export const filterForKind = ref<SelectList[]>([
   { name: '新作', value: 'new' },
   { name: 'CD', value: 'CD' },
   { name: '出版物', value: 'book' },
-  { name: '其他', value: '' },
+  { name: '其他', value: 'others' },
 ])
 export const filterForKindTem = ref<SelectList[]>([
   { name: '旧作', value: 'old' },
   { name: '新作', value: 'new' },
   { name: 'CD', value: 'CD' },
   { name: '出版物', value: 'book' },
-  { name: '其他', value: '' },
+  { name: '其他', value: 'others' },
 ])
 export function getFilterForKindTem(): void {
   filterForKindTem.value = JSON.parse(JSON.stringify(filterForKind.value))
@@ -88,7 +89,7 @@ export function resetFilterForKindTem(): void {
     { name: '新作', value: 'new' },
     { name: 'CD', value: 'CD' },
     { name: '出版物', value: 'book' },
-    { name: '其他', value: '' },
+    { name: '其他', value: 'others' },
   ]
 }
 
