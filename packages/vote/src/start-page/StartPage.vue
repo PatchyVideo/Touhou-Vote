ec8ec255837739e402310e4178c914412c076c8c
Lurantis <1510033076@qq.com>
2022-06-01 01:17:48 +0800

    chore(vote): :lipstick: update UI of vote start page

--- packages/vote/src/start-page/StartPage.vue
+++ packages/vote/src/start-page/StartPage.vue
@@ -3,8 +3,8 @@
   <div
     class="flex flex-wrap items-center w-full min-h-100vh px-3vh backdrop-filter backdrop-blur-1 md:mx-auto md:w-4/5 xl:w-1/2"
   >
-    <div class="space-y-6vh w-full md:pt-0 md:flex md:flex-wrap md:content-center md:space-y-4vh">
-      <div class="w-full space-y-2">
+    <div class="space-y-6vh w-full md:pt-0 md:flex md:flex-col md:flex-wrap md:content-center md:space-y-4vh">
+      <div class="md:w-1/2 space-y-2">
         <div class="quicksand text-gray-800 md:text-xl">
           <img class="inline-block w-5 h-6 pb-1 align-middle" src="https://thwiki.cc/favicon.ico" />THBWiki &
           <img class="inline-block w-8 h-10 pb-1 align-middle" src="@/common/assets/logoVoilelabs.png" />VoileLabs
@@ -14,10 +14,18 @@
           <div class="w-full"><img class="w-1/4 inline-block" src="@/common/assets/title10.svg" /></div>
         </div>
       </div>
-      <div>
-        <div>
-          {{ '距开始还有 ' + daysWith0 + '天' + hoursWith0 + '时' + minutesWith0 + '分' + secondsWith0 + '秒' }}
-        </div>
+      <div
+        class="md:text-xl xl:text-3xl 2xl:text-5xl 3xl:text-7xl flex space-x-0.5 md:space-x-1 2xl:space-x-2 items-end md:whitespace-nowrap"
+      >
+        <div>距开始还有</div>
+        <div class="text-2xl xl:text-5xl 2xl:text-7xl 3xl:text-8xl">{{ daysWith0 }}</div>
+        <div>天</div>
+        <div class="text-2xl xl:text-5xl 2xl:text-7xl 3xl:text-8xl">{{ hoursWith0 }}</div>
+        <div>时</div>
+        <div class="text-2xl xl:text-5xl 2xl:text-7xl 3xl:text-8xl">{{ minutesWith0 }}</div>
+        <div>分</div>
+        <div class="text-2xl xl:text-5xl 2xl:text-7xl 3xl:text-8xl">{{ secondsWith0 }}</div>
+        <div>秒</div>
       </div>
     </div>
     <div v-if="screenSizes['<md']"></div>
