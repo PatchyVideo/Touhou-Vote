e420eae320ac116063a9fdf35a4234045e2a73a9
Lurantis <1510033076@qq.com>
2022-07-02 20:22:22 +0800

    fix(vote): :bug: update vote ended time

--- packages/vote/src/end-page/lib/voteEnded.ts
+++ packages/vote/src/end-page/lib/voteEnded.ts
@@ -1,6 +1,6 @@
 // Deadline: 2021/7/4 00:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
-const deadline = new Date(2022, 6, 3).getTime()
+const deadline = new Date(2022, 6, 4).getTime()
 export const deadlineWithTimezoneOffset = deadline - 8 * 60 * 60 * 1000 - new Date().getTimezoneOffset() * 60 * 1000
 export function voteEnded(): boolean {
   return new Date(deadlineWithTimezoneOffset).getTime() < Date.now()
