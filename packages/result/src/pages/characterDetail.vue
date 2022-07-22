009d1a9d4a3aae48ae4732a4bd50c99f7d0c412f
Lurantis <1510033076@qq.com>
2022-07-22 20:16:25 +0800

    chore(result): :construction: sort for display name WIP

--- packages/result/src/pages/characterDetail.vue
+++ packages/result/src/pages/characterDetail.vue
@@ -2,10 +2,13 @@
   <div class="mx-1 my-3">
     <!-- Overview -->
     <div
-      class="mb-0 md:m-5 p-3 space-y-3 bg-white bg-opacity-80 rounded-t md:bg-opacity-0 md:rounded md:flex md:justify-between md:items-center">
+      class="mb-0 md:m-5 p-3 space-y-3 bg-white bg-opacity-80 rounded-t md:bg-opacity-0 md:rounded md:flex md:justify-between md:items-center"
+    >
       <div class="flex items-end">
-        <img src="https://upload.thwiki.cc/a/a6/THBWiki-LOGO-%E5%8D%9A%E4%B8%BD%E7%81%B5%E6%A2%A6%E6%96%B0%E4%BD%9C.png"
-          class="w-10 h-10 col-span-1 row-span-2 rounded" />
+        <img
+          src="https://upload.thwiki.cc/a/a6/THBWiki-LOGO-%E5%8D%9A%E4%B8%BD%E7%81%B5%E6%A2%A6%E6%96%B0%E4%BD%9C.png"
+          class="w-10 h-10 col-span-1 row-span-2 rounded"
+        />
         <h2 class="text-4xl font-light">角色部门</h2>
         <span class="ml-3 text-xl">结果信息</span>
       </div>
@@ -42,31 +45,40 @@
     <div class="md:mx-5 flex bg-white shadow rounded border border-accent-600">
       <!-- Fixed Header -->
       <div class="flex-grow flex">
-        <div v-for="item in headerFixed" :key="item.key" class="font-bold border-r border-accent-600"
-          :class="{ 'flex-grow': item.key === 'name' }">
+        <div
+          v-for="item in headerFixed"
+          :key="item.key"
+          class="font-bold border-r border-accent-600"
+          :class="{ 'flex-grow': item.key === 'name' }"
+        >
           <!-- Header -->
           <div class="py-1 px-3 whitespace-nowrap border-b border-accent-600">
-            <div class="i-uil:sort-amount-down"></div>
             <div>{{ item.name }}</div>
           </div>
           <!-- Content -->
           <div v-for="(item2, index) in resultCharacter" :key="item2.rank" class="relative">
-            <div class="p-1 max-w-30 md:max-w-none border-t border-accent-600 flex flex-nowrap items-center"
-              :class="{ 'pb-66': lineExpanded[index] }">
-              <div v-if="item.key === 'displayRank'" class="min-w-8"
+            <div
+              class="p-1 max-w-30 md:max-w-none border-t border-accent-600 flex flex-nowrap items-center"
+              :class="{ 'pb-66': lineExpanded[index] }"
+            >
+              <div
+                v-if="item.key === 'displayRank'"
+                class="min-w-8"
                 :class="lineExpanded[index] ? 'i-uil:minus' : 'i-uil:plus'"
-                @click="lineExpanded[index] = !lineExpanded[index]"></div>
+                @click="lineExpanded[index] = !lineExpanded[index]"
+              ></div>
               <div class="px-2 truncate">{{ item2[item.key] }}</div>
             </div>
             <!-- Folder -->
-            <div v-if="lineExpanded[index] && item.key === 'displayRank'"
-              class="absolute bottom-0 w-[calc(100vw-0.6rem)] md:w-[calc(100vw-4.1rem)] p-2 bg-white rounded shadow-inner border border-accent-600 space-y-2">
+            <div
+              v-if="lineExpanded[index] && item.key === 'displayRank'"
+              class="absolute bottom-0 w-[calc(100vw-0.6rem)] md:w-[calc(100vw-4.1rem)] p-2 bg-white rounded shadow-inner border border-accent-600 space-y-2"
+            >
               <div class="p-1 rounded border border-accent-600 divide-y divide-accent-300">
                 <div v-for="item3 in headerFolded" class="truncate py-1">
                   {{ item3.name + ': ' + item2[item3.key] }}
                 </div>
-                <div class="py-1">角色详情：<router-link class="cursor-pointer" to="/">点击这里</router-link>
-                </div>
+                <div class="py-1">角色详情：<router-link class="cursor-pointer" to="/">点击这里</router-link></div>
               </div>
             </div>
           </div>
@@ -74,13 +86,21 @@
       </div>
       <!-- Header not Fixed -->
       <div class="flex flex-nowrap overflow-auto">
-        <div v-for="item in headerWithoutFixed" :key="item.key" class="border-r border-accent-600">
+        <div v-for="item in headerWithoutFixed" :key="item.key" class="border-r border-accent-600 min-w-25">
           <!-- Header -->
-          <div class="py-1 px-3 whitespace-nowrap border-b border-accent-600">{{ item.name }}</div>
+          <div
+            class="p-1 whitespace-nowrap border-b border-accent-600 flex flex-nowrap justify-between items-center space-x-1"
+          >
+            <div>{{ item.name }}</div>
+            <div v-if="sortHeader.key === item.key" class="i-uil:sort-amount-down"></div>
+            <div v-else class="i-uil:arrows-v transition transition-colors text-white hover:text-black"></div>
+          </div>
           <!-- Content -->
           <div v-for="(item2, index) in resultCharacter" :key="item2.rank" class="relative">
-            <div class="py-1 px-3 truncate max-w-30 md:max-w-none border-t border-accent-600"
-              :class="{ 'pb-66': lineExpanded[index] }">
+            <div
+              class="py-1 px-3 truncate max-w-30 md:max-w-none border-t border-accent-600"
+              :class="{ 'pb-66': lineExpanded[index] }"
+            >
               {{ item2[item.key] }}
             </div>
           </div>
@@ -91,7 +111,9 @@
   <!-- Advanced Search -->
   <div
     class="shadow fixed bottom-10 right-5 bg-gray-50 cursor-pointer p-2 transition-opacity rounded-full dark:bg-gray-800"
-    title="筛选" @click="openSearch()">
+    title="筛选"
+    @click="openSearch()"
+  >
     <div class="i-uil:file-search-alt text-2xl" />
   </div>
   <characterDetailSearch v-model:open="search" />
@@ -106,7 +128,26 @@ import characterDetailSearch from '@/components/characterDetailSearch.vue'
 
 interface Header {
   name: string
-  key: 'rank' | 'displayRank' | 'name' | 'voteCount' | 'firstVoteCount' | 'firstVotePercentage' | 'firstVoteCountWeighted' | 'votePercentage' | 'firstPercentage' | 'maleVoteCount' | 'malePercentagePerChar' | 'femaleVoteCount' | 'femalePercentagePerChar' | 'nameJpn' | 'characterType' | 'characterOrigin' | 'firstAppearance' | 'malePercentagePerTotal' | 'femalePercentagePerTotal'
+  key:
+    | 'rank'
+    | 'displayRank'
+    | 'name'
+    | 'voteCount'
+    | 'firstVoteCount'
+    | 'firstVotePercentage'
+    | 'firstVoteCountWeighted'
+    | 'votePercentage'
+    | 'firstPercentage'
+    | 'maleVoteCount'
+    | 'malePercentagePerChar'
+    | 'femaleVoteCount'
+    | 'femalePercentagePerChar'
+    | 'nameJpn'
+    | 'characterType'
+    | 'characterOrigin'
+    | 'firstAppearance'
+    | 'malePercentagePerTotal'
+    | 'femalePercentagePerTotal'
 }
 const header: Header[] = [
   { name: '名次', key: 'displayRank' },
@@ -146,13 +187,45 @@ const headerWithoutFixed = computed<Header[]>(() =>
       !headerFixed.find((item2) => item2.key === item.key) && !headerFolded.find((item2) => item2.key === item.key)
   )
 )
-const sortheader = ref<{
-  key: 'rank' | 'displayRank' | 'name' | 'voteCount' | 'firstVoteCount' | 'firstVotePercentage' | 'firstVoteCountWeighted' | 'votePercentage' | 'firstPercentage' | 'maleVoteCount' | 'malePercentagePerChar' | 'femaleVoteCount' | 'femalePercentagePerChar' | 'nameJpn' | 'characterType' | 'characterOrigin' | 'firstAppearance' | 'malePercentagePerTotal' | 'femalePercentagePerTotal'
-  forward:boolean
+const sortHeader = ref<{
+  key:
+    | 'rank'
+    | 'displayRank'
+    | 'name'
+    | 'voteCount'
+    | 'firstVoteCount'
+    | 'firstVotePercentage'
+    | 'firstVoteCountWeighted'
+    | 'votePercentage'
+    | 'firstPercentage'
+    | 'maleVoteCount'
+    | 'malePercentagePerChar'
+    | 'femaleVoteCount'
+    | 'femalePercentagePerChar'
+    | 'nameJpn'
+    | 'characterType'
+    | 'characterOrigin'
+    | 'firstAppearance'
+    | 'malePercentagePerTotal'
+    | 'femalePercentagePerTotal'
+  forward: boolean
 }>({
-  key:'rank',
-  forward:true
+  key: 'rank',
+  forward: true,
 })
+
+watch(
+  sortHeader,
+  () => {
+    resultCharacter.value.sort((a, b) => {
+      if (sortHeader.value.key === 'firstVoteCount')
+        if (sortHeader.value.forward) return a[sortHeader.value.key] - b[sortHeader.value.key]
+        else return b[sortHeader.value.key] - a[sortHeader.value.key]
+      else return 0
+    })
+  },
+  { deep: true }
+)
 const totalUniqueItemsCharacter = ref(0)
 const totalFirstCharacter = ref(0)
 const totalVotesCharacter = ref(0)
@@ -264,7 +337,7 @@ queryRankingError((err) => {
   console.log(err.message)
 })
 function toPercentageString(num: number): string {
-  return (num*100).toFixed(2) + '%'
+  return (num * 100).toFixed(2) + '%'
 }
 
 const search = ref(false)
