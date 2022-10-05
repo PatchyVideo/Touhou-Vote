8bdf4c24f29e1f62fbe8aca3c10a86989255c546
Lurantis <1510033076@qq.com>
2022-10-05 20:17:23 +0800

    chore(result): :lipstick: update UI of homepage

--- packages/result/src/pages/index.vue
+++ packages/result/src/pages/index.vue
@@ -2,7 +2,7 @@
   <!-- Cover -->
   <div class="page"></div>
   <div class="flex justify-center">
-    <div class="flex w-full h-100vh flex-col">
+    <div class="flex w-full h-45vh md:h-70vh flex-col">
       <div class="flex-1"></div>
 
       <div ref="titleEl" class="flex-1 flex mx-2 my-auto max-h-32 justify-center items-center">
@@ -27,20 +27,24 @@
           <div>所有的投票结果将发布在本站内，请点击下面的链接进入相应的部分查看结果数据</div>
           <span class="text-sm italic text-gray-700">*请注意，一切在本站之外发表的排行、分析、视频均为非官方性质</span>
         </div>
-
-        <div class="flex w-full items-center text-gray-600 text-sm">
-          <div class="i-uil:arrow-down"></div>
-          继续向下滚动查看结果总览
-        </div>
       </div>
     </div>
   </div>
 
   <!-- Nav -->
-  <Nav no-padding :show-summary="isTitleVisible" />
-
-  <div class="mx-auto max-w-screen-lg min-h-100vh">
-    <SummaryCharacter />
+  <div class="my-5">
+    <Nav v-if="screenSizes['md']" no-padding />
+    <div v-else class="min-w-50 mx-auto space-y-1">
+      <router-link
+        :to="item.to"
+        v-for="item in resultTypeMeta"
+        :key="item.name"
+        class="flex items-center justify-center p-2 w-full max-w-100 border-y border-accent-400 bg-opacity-30 bg-accent-100 shadow cursor-pointer transition transition-all active:translate-1"
+      >
+        <img :src="item.img" class="w-10 h-10" />
+        <div class="text-black text-xl">{{ item.name }}</div>
+      </router-link>
+    </div>
   </div>
 
   <!-- Copyright -->
@@ -54,11 +58,37 @@
 </template>
 
 <script lang="ts" setup>
-const titleEl = shallowRef<HTMLDivElement | null>(null)
+import { screenSizes } from '@/composables/style'
 
 setSiteTitle('投票结果 - 第⑩回 中文东方人气投票')
 
-const isTitleVisible = useElementVisibility(titleEl)
+const resultTypeMeta = ref([
+  {
+    img: 'https://upload.thwiki.cc/thumb/a/a6/THBWiki-LOGO-%E5%8D%9A%E4%B8%BD%E7%81%B5%E6%A2%A6%E6%96%B0%E4%BD%9C.png/100px-THBWiki-LOGO-%E5%8D%9A%E4%B8%BD%E7%81%B5%E6%A2%A6%E6%96%B0%E4%BD%9C.png',
+    name: '角色部门',
+    to: '/character',
+  },
+  {
+    img: 'https://upload.thwiki.cc/thumb/4/49/THBWiki-LOGO-%E7%B1%B3%E6%96%AF%E8%92%82%E5%A8%85.png/100px-THBWiki-LOGO-%E7%B1%B3%E6%96%AF%E8%92%82%E5%A8%85.png',
+    name: '音乐部门',
+    to: '/music',
+  },
+  {
+    img: 'https://upload.thwiki.cc/thumb/a/a6/THBWiki-LOGO-%E7%A7%98%E5%B0%81%E4%BF%B1%E4%B9%90%E9%83%A8.png/100px-THBWiki-LOGO-%E7%A7%98%E5%B0%81%E4%BF%B1%E4%B9%90%E9%83%A8.png',
+    name: 'CP部门',
+    to: '/couple',
+  },
+  {
+    img: 'https://upload.thwiki.cc/thumb/f/f7/THBWiki-LOGO-%E5%AE%87%E4%BD%90%E8%A7%81%E5%A0%87%E5%AD%90.png/100px-THBWiki-LOGO-%E5%AE%87%E4%BD%90%E8%A7%81%E5%A0%87%E5%AD%90.png',
+    name: '提名部门',
+    to: 'doujin',
+  },
+  {
+    img: 'https://upload.thwiki.cc/thumb/d/dd/THBWiki-LOGO-%E6%9C%AC%E5%B1%85%E5%B0%8F%E9%93%83.png/100px-THBWiki-LOGO-%E6%9C%AC%E5%B1%85%E5%B0%8F%E9%93%83.png',
+    name: '调查问卷',
+    to: 'questionnaire',
+  },
+])
 </script>
 
 <style lang="postcss" scoped></style>
