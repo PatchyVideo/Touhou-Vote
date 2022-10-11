0be97df0ff84d17cad6ea6de1bc9158aa689feab
Lurantis <1510033076@qq.com>
2022-10-11 22:56:00 +0800

    chore(result): :construction: couple single detail page WIP

---
+++ packages/result/src/pages/CoupleSingleDetail.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="mx-1 my-3">
+    <!-- Overview -->
+    <div
+      class="mb-0 md:mx-5 p-3 space-y-3 bg-white bg-opacity-80 rounded-t md:bg-opacity-0 md:rounded md:flex md:flex-wrap md:justify-between md:items-center"
+    >
+      <div class="flex items-center">
+        <img
+          src="https://upload.thwiki.cc/thumb/a/a6/THBWiki-LOGO-%E7%A7%98%E5%B0%81%E4%BF%B1%E4%B9%90%E9%83%A8.png/100px-THBWiki-LOGO-%E7%A7%98%E5%B0%81%E4%BF%B1%E4%B9%90%E9%83%A8.png"
+          class="w-10 h-10 col-span-1 row-span-2 rounded"
+        />
+        <div>
+          <h2 class="text-3xl font-light">CP信息</h2>
+          <span class="text-xl hidden md:inline-block">{{ coupleName }}</span>
+        </div>
+      </div>
+      <span class="text-xl md:hidden">{{ coupleName }}</span>
+
+      <div class="grid grid-cols-3 md:grid-cols-6 gap-1 text-sm md:text-base text-center">
+        <div>
+          <div>票数</div>
+          <div>{{ voteCount }}</div>
+        </div>
+        <div>
+          <div>本命票数</div>
+          <div>{{ firstVoteCount }}</div>
+        </div>
+        <div>
+          <div>本命率</div>
+          <div>{{ firstVotePercentage }}</div>
+        </div>
+        <div>
+          <div>票数全局占比</div>
+          <div>{{ votePercentage }}</div>
+        </div>
+        <div>
+          <div>本命全局占比</div>
+          <div>{{ firstPercentage }}</div>
+        </div>
+        <div>
+          <div>投票理由</div>
+          <router-link v-if="numReasons > 0" class="underline" :to="'/coupleReason?rank=' + coupleRank">{{
+            numReasons + '(点此查看)'
+          }}</router-link>
+          <div v-else>{{ numReasons }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="md:mx-5 p-3 py-1 bg-white bg-opacity-80 rounded-b md:bg-opacity-0 text-sm italic text-gray-700">
+      * 本页面为单独CP的详细信息页面，下面每个栏目的内容分别有各自的说明<br />
+    </div>
+    <div class="md:mx-5 p-3">
+      <div class="text-2xl py-0.5 border-b border-accent-600">角色与主动方倾向信息信息</div>
+    </div>
+    <div class="md:mx-5 p-3"></div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+import { gql, useQuery } from '@/composables/graphql'
+import type { Query } from '@/composables/graphql'
+import NProgress from 'nprogress'
+import { toPercentageString } from '@/lib/numberFormat'
+
+const route = useRoute()
+
+const coupleRank = ref(
+  Number(route.query.rank ? (Array.isArray(route.query.rank) ? route.query.rank[0] : route.query.rank) : 1)
+)
+const coupleName = ref('ID：' + coupleRank.value)
+const voteCount = ref(-1)
+const firstVoteCount = ref(-1)
+const firstVotePercentage = ref<number | string>(-1)
+const votePercentage = ref<number | string>(-1)
+const firstPercentage = ref<number | string>(-1)
+const numReasons = ref(-1)
+const { result, loading, onError } = useQuery<Query>(
+  gql`
+    query ($voteStart: DateTimeUtc!, $voteYear: Int!, $rank: Int!) {
+      queryCPSingle(voteStart: $voteStart, voteYear: $voteYear, rank: $rank) {
+        cp {
+          a
+          b
+          c
+        }
+        aActive
+        bActive
+        cActive
+        noneActive
+        voteCount
+        firstVoteCount
+        firstVotePercentage
+        votePercentage
+        firstPercentage
+        numReasons
+      }
+    }
+  `,
+  {
+    voteStart: new Date(Date.UTC(2022, 5, 17, 10)),
+    voteYear: 10,
+    rank: coupleRank.value,
+  }
+)
+watchEffect(() => {
+  if (loading.value) {
+    if (!NProgress.isStarted()) NProgress.start()
+  } else {
+    if (NProgress.isStarted()) NProgress.done()
+  }
+})
+watchEffect(() => {
+  if (result.value) {
+    if (result.value.queryCPSingle) {
+      coupleName.value =
+        result.value.queryCPSingle.cp.a +
+        ' x ' +
+        result.value.queryCPSingle.cp.b +
+        (result.value.queryCPSingle.cp.c ? ' x ' + result.value.queryCPSingle.cp.c : '')
+      setSiteTitle(coupleName.value + ' - 第⑩回 中文东方人气投票')
+      voteCount.value = result.value.queryCPSingle.voteCount
+      firstVoteCount.value = result.value.queryCPSingle.firstVoteCount
+      firstVotePercentage.value = toPercentageString(result.value.queryCPSingle.firstVotePercentage)
+      votePercentage.value = toPercentageString(result.value.queryCPSingle.votePercentage)
+      firstPercentage.value = toPercentageString(result.value.queryCPSingle.firstPercentage)
+      numReasons.value = result.value.queryCPSingle.numReasons
+    }
+  }
+})
+onError((err) => {
+  alert(err.message)
+  console.log(err.message)
+})
+</script>
+<route lang="yaml">
+meta:
+  navid: couple
+</route>
