0a18c4415fbbe6f1dcaa200ca24342589da46ac3
Lurantis <1510033076@qq.com>
2022-10-21 21:39:29 +0800

    chore(result): update graph interface

--- packages/result/src/lib/Graph.ts
+++ packages/result/src/lib/Graph.ts
@@ -1,19 +1,19 @@
-import { deadline, startTime } from '@touhou-vote/shared/data/time'
+import { deadline, startTime, timeFormat } from '@touhou-vote/shared/data/time'
 import type { VotingTrendItem } from '@/composables/graphql/__generated__/graphql'
 const hourOfMs = 60 * 60 * 1000
 export const GraphTimeRange = (() => {
   const range: string[] = []
   for (let timer = startTime + hourOfMs; timer <= deadline; timer = timer + hourOfMs) {
-    const timePoint = new Date(timer)
-    range.push(timePoint.getMonth() + 1 + '-' + timePoint.getDate() + ' ' + timePoint.getHours() + ': 00')
+    range.push(timeFormat(new Date(timer)))
   }
   return range
 })()
+
 export interface GraphDataLine {
   name: string
   data: number[]
 }
-export function getTrendData(name: string, trend: VotingTrendItem[]): GraphDataLine {
+export function getAddedTrendData(name: string, trend: VotingTrendItem[]): GraphDataLine {
   const trendData: GraphDataLine = {
     name: name,
     data: [],
@@ -22,8 +22,30 @@ export function getTrendData(name: string, trend: VotingTrendItem[]): GraphDataL
   for (let i = 0; i < trend.length; i++) {
     trendData.data[trend[i].hrs] = trend[i].cnt
   }
+  return trendData
+}
+export function getTrendData(name: string, trend: VotingTrendItem[]): GraphDataLine {
+  const trendData: GraphDataLine = getAddedTrendData(name, trend)
   for (let i = 1; i < trendData.data.length; i++) {
     trendData.data[i] = trendData.data[i - 1] + trendData.data[i]
   }
   return trendData
+}
+
+export interface GraphDataPie {
+  name: string
+  value: number
+}
+export interface GraphDataSunburst {
+  id: string
+  name: string
+  value: number
+}
+export interface GraphDataRadar {
+  name: string
+  value: number[]
+}
+export interface GraphDataMap {
+  name: string
+  value: number
 }
