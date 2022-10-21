fe497b001e1aef82ec9ea750d00e0ef2022e5a16
Lurantis <1510033076@qq.com>
2022-10-21 21:40:04 +0800

    chore(result): update time related functions

--- packages/shared/data/time.ts
+++ packages/shared/data/time.ts
@@ -4,3 +4,8 @@ export const startTime = new Date(2022, 5, 17, 18).getTime()
 // Deadline: 2021/7/5 00:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
 export const deadline = new Date(2022, 6, 5).getTime()
+export function timeFormat(date: Date): string {
+  return date.getMonth() + 1 + '-' + date.getDate() + ' ' + date.getHours() + ': 00'
+}
+export const startTimeString = computed<string>(() => timeFormat(new Date(startTime)))
+export const deadlineString = computed<string>(() => timeFormat(new Date(deadline)))
