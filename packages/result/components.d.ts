57988bc9d6b80ad4d3a8101647b0d7da31791955
Lurantis <1510033076@qq.com>
2022-10-22 16:10:59 +0800

    refactor(result): :zap: draw questionnaire component

--- packages/result/components.d.ts
+++ packages/result/components.d.ts
@@ -16,6 +16,7 @@ declare module '@vue/runtime-core' {
     GraphSunburst: typeof import('./src/components/GraphSunburst.vue')['default']
     Nav: typeof import('./src/components/Nav.vue')['default']
     NavTop: typeof import('./src/components/NavTop.vue')['default']
+    Questionnaire: typeof import('./src/components/Questionnaire.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SummaryCharacter: typeof import('./src/components/summary/Character.vue')['default']
--- packages/result/src/pages/characterSingleDetail.vue
+++ packages/result/src/pages/characterSingleDetail.vue
@@ -52,122 +52,21 @@
       </div>
       <GraphEvolution :x-axis="GraphTimeRange" :data="trend" class="max-w-4xl pt-3 mx-auto" />
     </div>
-    <!-- Questionnaire Complete -->
-    <div class="md:mx-5 p-3">
-      <div class="text-2xl py-0.5 border-b border-accent-600">投票群体 - 问卷填写情况</div>
-      <GraphPie :data="questionnaireCompletionRates" :much-legend="true" class="max-w-4xl pt-3 mx-auto" />
-    </div>
-    <!-- Vote Group -->
-    <div class="md:mx-5 p-3">
-      <div class="text-2xl py-0.5 mb-3 border-b border-accent-600">投票群体 - 问卷回答情况</div>
-      <div v-for="item in questionItem" :key="item.questionId" class="mb-3">
-        <div class="py-0.5 border-b border-accent-300">
-          {{ item.question }}
-        </div>
-        <div class="py-1 text-sm">
-          {{ '总回答数：' + item.totalAnswers + '；男性数：' + item.totalMale + '；女性数：' + item.totalFemale }}
-        </div>
-        <GraphMap
-          v-if="item.questionId === 'q11031'"
-          :data-total="
-            item.answersCat.map((item2) => {
-              return {
-                name: item2.content,
-                value: item2.totalVotes,
-              }
-            })
-          "
-          :data-male="
-            item.answersCat.map((item2) => {
-              return {
-                name: item2.content,
-                value: item2.maleVotes,
-              }
-            })
-          "
-          :data-female="
-            item.answersCat.map((item2) => {
-              return {
-                name: item2.content,
-                value: item2.femaleVotes,
-              }
-            })
-          "
-          class="max-w-4xl pt-3 mx-auto"
-        />
-        <GraphPie
-          v-else-if="item.questionId === 'q11011'"
-          :data="
-            item.answersCat.map((item2) => {
-              return {
-                name: item2.content,
-                value: item2.totalVotes,
-              }
-            })
-          "
-          class="max-w-4xl pt-3 mx-auto"
-        />
-        <div v-else-if="item.type === 'Single'">
-          <GraphSunburst
-            :data="
-              item.answersCat.map((item2) => {
-                return {
-                  id: item2.aid,
-                  name: item2.content,
-                  value: item2.totalVotes,
-                }
-              })
-            "
-            :data-out="getMaleAndFemaleAnswerNumber(item)"
-            :much-legend="item.answersCat.length > 5"
-            class="max-w-4xl pt-3 mx-auto"
-          />
-        </div>
-        <GraphRadar
-          :indicator="getIndicator(item)"
-          :data="getDataRadar(item)"
-          v-else-if="item.type === 'Multiple'"
-          class="max-w-4xl pt-3 mx-auto"
-        />
-        <router-link
-          class="py-3 inline-block"
-          :to="'QuestionnaireInputDetail?qid=' + item.questionId + '&q=chars:[&quot;' + characterName + '&quot;]'"
-          v-else-if="item.type === 'Input'"
-          >点击这里查看全部回答</router-link
-        >
-      </div>
-    </div>
+    <Questionnaire class="md:mx-5" :q="q" />
   </div>
 </template>
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
-import { gql, useQuery, useLazyQuery } from '@/composables/graphql'
+import { gql, useQuery } from '@/composables/graphql'
 import type { Query } from '@/composables/graphql'
 import NProgress from 'nprogress'
 import { characterList } from '@touhou-vote/shared/data/character'
 import { toPercentageString } from '@/lib/numberFormat'
-import {
-  GraphDataLine,
-  GraphTimeRange,
-  getTrendData,
-  getAddedTrendData,
-  GraphDataPie,
-  GraphDataSunburst,
-  GraphDataRadar,
-} from '@/lib/Graph'
-import {
-  allQuestionnaireIDList,
-  qIDToID,
-  findQuestionWithQuestionID,
-  getSmallQuestionnaireChineseName,
-} from '@/lib/Questionnaire'
+import { GraphDataLine, GraphTimeRange, getTrendData, getAddedTrendData } from '@/lib/Graph'
 import { startTimeString, deadlineString } from '@touhou-vote/shared/data/time'
 import characterImages from '@/assets/defaultCharacterImage.png?url'
-import GraphMap from '../components/GraphMap.vue'
-import GraphPie from '../components/GraphPie.vue'
-import GraphSunburst from '../components/GraphSunburst.vue'
-import GraphRadar from '../components/GraphRadar.vue'
-import GraphEvolution from '../components/GraphEvolution.vue'
+import GraphEvolution from '@/components/GraphEvolution.vue'
+import Questionnaire from '@/components/Questionnaire.vue'
 
 const route = useRoute()
 
@@ -185,6 +84,7 @@ const votePercentage = ref<number | string>(-1)
 const firstPercentage = ref<number | string>(-1)
 const numReasons = ref(-1)
 const trend = ref<GraphDataLine[]>([])
+const q = ref<string>('NONE')
 const { result, loading, onError } = useQuery<Query>(
   gql`
     query ($voteStart: DateTimeUtc!, $voteYear: Int!, $rank: Int!) {
@@ -218,7 +118,7 @@ watchEffect(() => {
     if (result.value.queryCharacterSingle) {
       characterName.value = result.value.queryCharacterSingle.name
       setSiteTitle(characterName.value + ' - 第⑩回 中文东方人气投票')
-      QuestionnaireDetail(characterName.value)
+      q.value = 'chars:["' + characterName.value + '"]'
       voteCount.value = result.value.queryCharacterSingle.voteCount
       firstVoteCount.value = result.value.queryCharacterSingle.firstVoteCount
       firstVotePercentage.value = toPercentageString(result.value.queryCharacterSingle.firstVotePercentage)
@@ -238,166 +138,8 @@ onError((err) => {
   alert(err.message)
   console.log(err.message)
 })
-
-const questionnaireCompletionRates = ref<GraphDataPie[]>([])
-interface QuestionItem {
-  questionId: string
-  question: string
-  type: 'Input' | 'Single' | 'Multiple'
-  answersCat: {
-    aid: string
-    content: string
-    totalVotes: number
-    maleVotes: number
-    femaleVotes: number
-  }[]
-  totalAnswers: number
-  totalMale: number
-  totalFemale: number
-}
-const questionItem = ref<QuestionItem[]>([])
-function getMaleAndFemaleAnswerNumber(item: QuestionItem): GraphDataSunburst[] {
-  const dataOut: GraphDataSunburst[] = []
-  item.answersCat.map((item2) => {
-    dataOut.push(
-      {
-        id: item2.aid + '0',
-        name: '男性',
-        value: item2.maleVotes,
-      },
-      {
-        id: item2.aid + '1',
-        name: '女性',
-        value: item2.femaleVotes,
-      }
-    )
-  })
-  return dataOut
-}
-function getIndicator(item: QuestionItem): string[] {
-  const indicator: string[] = []
-  item.answersCat.map((item2) => indicator.push(item2.content))
-  return indicator
-}
-function getDataRadar(item: QuestionItem): GraphDataRadar[] {
-  const data = []
-  data.push(
-    { name: '总票数2', value: item.answersCat.map((item2) => item2.totalVotes) },
-    { name: '男性票数', value: item.answersCat.map((item2) => item2.maleVotes) },
-    { name: '女性票数', value: item.answersCat.map((item2) => item2.femaleVotes) }
-  )
-  return data
-}
-const {
-  result: queryQuestionnaireResult,
-  load: loadQuestionnaire,
-  loading: queryQuestionnaireLoading,
-  onError: queryQuestionnaireError,
-  fetchMore: queryQuestionnaireMore,
-  forceDisabled: queryQuestionnaireDisabled,
-} = useLazyQuery<Query>(
-  gql`
-    query ($voteStart: DateTimeUtc!, $voteYear: Int!, $query: String, $questionsOfInterest: [String!]!) {
-      queryQuestionnaire(
-        voteStart: $voteStart
-        voteYear: $voteYear
-        query: $query
-        questionsOfInterest: $questionsOfInterest
-      ) {
-        entries {
-          questionId
-          answersCat {
-            aid
-            totalVotes
-            maleVotes
-            femaleVotes
-          }
-          totalAnswers
-          totalMale
-          totalFemale
-        }
-      }
-      queryCompletionRates(voteStart: $voteStart, voteYear: $voteYear, query: $query) {
-        items {
-          name
-          numComplete
-        }
-      }
-    }
-  `,
-  {
-    voteStart: new Date(Date.UTC(2022, 5, 17, 10)),
-    voteYear: 10,
-    query: 'chars:["' + characterName.value + '"]',
-    questionsOfInterest: allQuestionnaireIDList.value,
-  }
-)
 watchEffect(() => {
-  if (queryQuestionnaireResult.value) {
-    if (queryQuestionnaireResult.value.queryQuestionnaire) {
-      questionItem.value = queryQuestionnaireResult.value.queryQuestionnaire.entries
-        .map((item) => {
-          return {
-            questionId: item.questionId,
-            question: findQuestionWithQuestionID(qIDToID(item.questionId)).question,
-            type: findQuestionWithQuestionID(qIDToID(item.questionId)).type,
-            answersCat: item.answersCat.map((item2) => {
-              return {
-                aid: item2.aid,
-                content:
-                  findQuestionWithQuestionID(qIDToID(item.questionId)).options.find(
-                    (item3) => String(item3.id) === item2.aid
-                  )?.content || 'ERROR',
-                totalVotes: item2.totalVotes,
-                maleVotes: item2.maleVotes,
-                femaleVotes: item2.femaleVotes,
-              }
-            }),
-            totalAnswers: item.totalAnswers,
-            totalMale: item.totalMale,
-            totalFemale: item.totalFemale,
-          }
-        })
-        .sort((a, b) => Number(qIDToID(a.questionId)) - Number(qIDToID(b.questionId)))
-    }
-    if (queryQuestionnaireResult.value.queryCompletionRates) {
-      questionnaireCompletionRates.value = queryQuestionnaireResult.value.queryCompletionRates.items.map((item) => {
-        return {
-          name: getSmallQuestionnaireChineseName(item.name),
-          value: item.numComplete,
-        }
-      })
-    }
-  }
-})
-queryQuestionnaireError((err) => {
-  alert(err.message)
-  console.log(err.message)
-})
-async function QuestionnaireDetail(name: string): Promise<void> {
-  if (queryQuestionnaireDisabled.value)
-    loadQuestionnaire(undefined, {
-      voteStart: new Date(Date.UTC(2022, 5, 17, 10)),
-      voteYear: 10,
-      query: 'chars:["' + name + '"]',
-      questionsOfInterest: allQuestionnaireIDList.value,
-    })
-  else
-    queryQuestionnaireMore({
-      variables: {
-        voteStart: new Date(Date.UTC(2022, 5, 17, 10)),
-        voteYear: 10,
-        query: 'chars:["' + name + '"]',
-        questionsOfInterest: allQuestionnaireIDList.value,
-      },
-      updateQuery(previousQueryResult, { fetchMoreResult }) {
-        if (!fetchMoreResult) return previousQueryResult
-        return fetchMoreResult
-      },
-    })
-}
-watchEffect(() => {
-  if (loading.value || queryQuestionnaireLoading.value) {
+  if (loading.value) {
     if (!NProgress.isStarted()) NProgress.start()
   } else {
     if (NProgress.isStarted()) NProgress.done()
