4e39dfae2c0353fdc27538a12fa6911eca87a197
Lurantis <1510033076@qq.com>
2022-10-22 22:54:29 +0800

    chore: update vote time format

--- packages/shared/data/time.ts
+++ packages/shared/data/time.ts
@@ -5,7 +5,7 @@ export const startTime = new Date(2022, 5, 17, 18).getTime()
 // Notice that month start at "0", not "1", so January is "0"
 export const deadline = new Date(2022, 6, 5).getTime()
 export function timeFormat(date: Date): string {
-  return date.getMonth() + 1 + '-' + date.getDate() + ' ' + date.getHours() + ': 00'
+  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ': 00'
 }
 export const startTimeString = computed<string>(() => timeFormat(new Date(startTime)))
 export const deadlineString = computed<string>(() => timeFormat(new Date(deadline)))
