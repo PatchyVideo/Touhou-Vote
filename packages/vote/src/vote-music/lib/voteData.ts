dbfb75b4d0d2b0bb2a3dbac87fceb510630689e6
Lurantis <1510033076@qq.com>
2023-06-26 22:32:34 +0800

    perf(vote): :zap: update key from 'name' to 'id' in vote music

--- packages/vote/src/vote-music/lib/voteData.ts
+++ packages/vote/src/vote-music/lib/voteData.ts
@@ -3,16 +3,21 @@ import { Music } from '@/vote-music/lib/music'
 import { musicList } from '@/vote-music/lib/musicList'
 import type { MusicSubmitQuery } from '@/graphql/__generated__/graphql'
 
+// Honmei data, as New Music if no data
+// READONLY
+export const MUSICVOTENUM = 12
+
+// Vote data, including blank ticket seat (as New Music)
 export const musicHonmei = computed<Music>(() => musics.value.find((music) => music.honmei) || new Music())
 
-export const musics = ref<Music[]>(new Array(12).fill(null).map(() => new Music()))
-export function updateVotemusics(musicVoteData: MusicSubmitQuery[]): void {
-  if (!musicVoteData.length) return
-  musics.value = new Array(12).fill(null).map(() => new Music())
-  for (let i = 0; i < musicVoteData.length; i++) {
-    const musicData = musicList.find((item) => item.name === musicVoteData[i].name) || musics.value[i]
-    musicData.honmei = musicVoteData[i].first || false
-    musicData.reason = musicVoteData[i].reason || ''
+export const musics = ref<Music[]>(new Array(MUSICVOTENUM).fill(null).map(() => new Music()))
+export function updateVoteMusics(newVoteData: MusicSubmitQuery[]): void {
+  if (!newVoteData.length) return
+  musics.value = new Array(MUSICVOTENUM).fill(null).map(() => new Music())
+  for (let i = 0; i < newVoteData.length; i++) {
+    const musicData = musicList.find((item) => item.name === newVoteData[i].name) || musics.value[i]
+    musicData.honmei = newVoteData[i].first || false
+    musicData.reason = newVoteData[i].reason || ''
     musics.value[i] = musicData
   }
 }
