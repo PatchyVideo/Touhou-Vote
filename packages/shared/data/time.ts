a109587e8d4c4b82bb6ead656c15c71bca0f847c
Lurantis <1510033076@qq.com>
2023-09-28 08:50:33 +0800

    test: :white_check_mark: update graphql URL and start time (for test)

--- packages/shared/data/time.ts
+++ packages/shared/data/time.ts
@@ -3,7 +3,7 @@
 export const startTime = new Date(2023, 5, 17, 18).getTime()
 // Deadline: 2021/7/5 00:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
-export const deadline = new Date(2023, 9, 5).getTime()
+export const deadline = new Date(2023, 11, 5).getTime()
 export function timeFormat(date: Date): string {
   return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ': 00'
 }
