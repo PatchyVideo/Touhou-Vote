a139ef9e44801ea3d9166bfa31ef774e558f0797
Lurantis <1510033076@qq.com>
2023-10-14 23:49:38 +0800

    chore(vote): :speech_balloon: update copyright

--- packages/vote/src/common/components/Copyright.vue
+++ packages/vote/src/common/components/Copyright.vue
@@ -2,7 +2,7 @@
   <div
     :class="{ 'fixed bottom-0 left-0': props.copyrightType === 'fixed', 'absolute w-100vw left-0 bottom-1': props.copyrightType === 'absolute' }"
     class="w-full text-center my-6">
-    &copy; Copyright 2022 - 2023 THBWiki, VoileLabs, All rights reserved. Licensed under GPL-3.0.&ensp;
+    Copyright &copy; 2022 - 2023 THBWiki, VoileLabs. All rights reserved. Licensed under GPL-3.0.&ensp;
     <div>
       <a target="_blank" rel="noopener noreferrer" href="https://jq.qq.com/?k=0BnkgDKx">反馈问题</a>&ensp;
       <a target="_blank" rel="noopener noreferrer" href="/nav">往届结果</a>
