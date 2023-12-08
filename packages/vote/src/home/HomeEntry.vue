b38811aca0204fdd1116be4e3fe0e4bea6641463
Lurantis <1510033076@qq.com>
2023-12-08 09:14:58 +0800

    chore(vote): :fire: remove result page in vote

--- packages/vote/src/home/HomeEntry.vue
+++ packages/vote/src/home/HomeEntry.vue
@@ -1,24 +1,10 @@
 <template>
   <Component
-    :is="
-      voteNotStart()
-        ? StartPage
-        : voteEnded()
-        ? resultPage
-          ? Result
-          : isLogin
-          ? UserSettings
-          : EndPage
-        : isLogin
-        ? UserHome
-        : Home
-    "
+    :is="voteNotStart() ? StartPage : voteEnded() ? (isLogin ? UserSettings : EndPage) : isLogin ? UserHome : Home"
   />
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
 import { isLogin } from '@/home/lib/user'
 import { voteNotStart } from '@/start-page/lib/voteStart'
 import { voteEnded } from '@/end-page/lib/voteEnded'
@@ -27,15 +13,4 @@ import UserHome from './UserHome.vue'
 import UserSettings from './UserSettings.vue'
 import StartPage from '@/start-page/StartPage.vue'
 import EndPage from '@/end-page/EndPage.vue'
-import Result from '@/result/Result.vue'
-
-const route = useRoute()
-
-const resultPage = computed<boolean>(() => {
-  return route.query.result
-    ? Array.isArray(route.query.result)
-      ? Boolean(route.query.result[0])
-      : Boolean(route.query.result)
-    : false
-})
 </script>
