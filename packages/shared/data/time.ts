3c8c62bd1007f8fbc724c133e20660e20fdf26ae
Lurantis <1510033076@qq.com>
2023-12-23 18:59:54 +0800

    chore: update vote time

--- packages/shared/data/time.ts
+++ packages/shared/data/time.ts
@@ -1,9 +1,9 @@
-// Strat Time: 2021/6/17 18:00:00 UTC+8
+// Strat Time: 2023/12/29 18:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
-export const startTime = new Date(2023, 5, 17, 18).getTime()
-// Deadline: 2021/7/5 00:00:00 UTC+8
+export const startTime = new Date(2022, 11, 29, 18).getTime()
+// Deadline: 2024/1/15 00:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
-export const deadline = new Date(2024, 11, 5).getTime()
+export const deadline = new Date(2024, 0, 15).getTime()
 export function timeFormat(date: Date): string {
   return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ': 00'
 }
