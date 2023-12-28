a73e99db21c4cd82a34b7d5e23b34f8c99ee4cd6
Lurantis <1510033076@qq.com>
2023-12-29 02:04:41 +0800

    test(vote): update time for test

--- packages/shared/data/time.ts
+++ packages/shared/data/time.ts
@@ -1,6 +1,6 @@
 // Strat Time: 2023/12/29 18:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
-export const startTime = new Date(2023, 11, 29, 18).getTime()
+export const startTime = new Date(2022, 11, 29, 18).getTime()
 // Deadline: 2024/1/15 00:00:00 UTC+8
 // Notice that month start at "0", not "1", so January is "0"
 export const deadline = new Date(2024, 0, 15).getTime()
