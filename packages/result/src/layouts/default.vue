8f427f230679d0a54d6091876984bd2dd42902f9
Lurantis <1510033076@qq.com>
2024-01-03 23:23:40 +0800

    chore(result): :lipstick: update copyright

--- packages/result/src/layouts/default.vue
+++ packages/result/src/layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <Nav no-top />
   <div class="max-w-450 mx-auto"><RouterView /></div>
-  <div class="w-full text-center">
-    <div>
-      <a rel="noopener noreferrer" href="/nav">历届人气投票结果</a>&ensp;
-      <a target="_blank" rel="noopener noreferrer" href="https://jq.qq.com/?k=0BnkgDKx">反馈问题</a>
+  <div class="w-full text-center my-6">
+    <div class="quicksand">
+      Copyright &copy; 2022 - 2023 THBWiki, VoileLabs. All rights reserved. Licensed under GPL-3.0.&ensp;
     </div>
-    <div class="quicksand">&copy; Copyright 2022 THBWiki, VoileLabs. Licensed under GPL-3.0.</div>
+    <div>
+      <a target="_blank" rel="noopener noreferrer" href="https://jq.qq.com/?k=0BnkgDKx">反馈问题</a>&ensp;
+      <a target="_blank" rel="noopener noreferrer" href="/nav">往届结果</a>
+    </div>
   </div>
 </template>
